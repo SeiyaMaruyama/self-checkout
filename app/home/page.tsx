8a0e6a2febad76b4da80461995e781334294a563
sacradius <s256255@wakayama-u.ac.jp>
2023-11-01 09:17:23 +0900

    とりあえずbuildが通るようにした(homeからのページ遷移を実装する必要あり)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,13 @@
 // import Image from 'next/image'
 import * as React from 'react';
 import { Button } from '@mui/material';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="homeContaints">
       <h1 className="title">セルフレジのデモ</h1>
-      <Button variant="outlined" size="large" component={Link} to="app/(select)/page.tsx">
+      <Button variant="outlined" size="large" /*component={Link} to="/app/select/page.tsx"*/>
         スタート
       </Button>
     </div>
