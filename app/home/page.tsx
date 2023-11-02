6b07e00ef58a00b664bd131e0ccdb4042b581f6f
sacradius <s256255@wakayama-u.ac.jp>
2023-11-02 15:59:43 +0900

    ホーム画面のプロト

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,11 +8,8 @@ import * as React from 'react';
 import { Button } from '@mui/material';
 // import { Link } from "react-router-dom";
 
-export default function Home() {
-    return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div className="text-center">
+/*
+*
                     <img
                         alt="Company Logo"
                         className="mx-auto h-12 w-auto"
@@ -24,6 +21,16 @@ export default function Home() {
                         }}
                         width={72}
                     />
+*
+* */
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+                <div className="text-center">
+
+
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
                         セルフレジを開始する
                     </h2>
