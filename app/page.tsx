dd84ec6fbeb4772e07662f5825d1de43b059406c
sacradius <s256255@wakayama-u.ac.jp>
2023-11-17 12:17:19 +0900

    遷移の調整

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
                 </div>
                 <div className="mt-8 space-y-6">
                     <div>
-                        <Link href={"/select"}>
+                        <Link href={"/"}>
                             <Button
                                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 type="submit"
--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -10,10 +10,9 @@ export default function Component() {
         <section className="w-full h-screen flex flex-col items-center justify-center space-y-8 bg-gray-100 dark:bg-gray-900">
             <div className="text-3xl font-bold text-gray-800 dark:text-gray-200">Step-by-step Tutorial</div>
             <ol className="list-decimal list-inside space-y-4 text-lg text-gray-700 dark:text-gray-300">
-                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 1: Do something</li>
-                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 2: Do something else</li>
-                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 3: Do something different</li>
-                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 4: Complete the process</li>
+                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 1: 商品を選択</li>
+                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 2: カートを確認</li>
+                <li className="bg-gray-200 dark:bg-gray-800 p-4 rounded">Step 3: 決済画面へ</li>
             </ol>
             <div className="flex space-x-4">
                 <Link href={"/"}>
