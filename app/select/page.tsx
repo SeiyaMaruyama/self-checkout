2289cacf3f4b10db84ded0e879b7e15f294ab819
sacradius <s256255@wakayama-u.ac.jp>
2023-11-18 02:25:01 +0900

    タブの機能実装

--- app/select/page.tsx
+++ app/select/page.tsx
@@ -3,7 +3,7 @@
  * @see https://v0.dev/t/AC6YqcoATmG
  */
 
-"use client"
+"use client";
 
 import React, { useState } from "react";
 // import prisma from "@/lib/prisma";
@@ -13,15 +13,16 @@ import Link from "next/link";
 type Product = {
   name: string;
   price: number;
-}
+};
 
 type CartItem = {
   product: Product;
   quantity: number;
-}
+};
 
 const SelectComponent = () => {
   const [cart, setCart] = useState<CartItem[]>([]);
+  const [selectedTab, setSelectedTab] = useState("お菓子");
   const [startTime, setStartTime] = useState(new Date());
   const [time, setTime] = useState<number>(0);
   // const [measurementId, setMeasurementId] = useState<string | null>(null);
@@ -29,7 +30,7 @@ const SelectComponent = () => {
   // const prisma = new PrismaClient();
 
   //useEffect(() => {
-    /*
+  /*
     const createMeasurement = async () => {
       const audio = new Audio('public/Nice_Wake_Up.mp3');
       await audio.play();
@@ -40,12 +41,14 @@ const SelectComponent = () => {
 
   const addToCart = (product: Product, quantity: number = 1) => {
     setCart((currentCart) => {
-      const itemInCart = currentCart.find((item) => item.product.name === product.name);
+      const itemInCart = currentCart.find(
+        (item) => item.product.name === product.name,
+      );
       if (itemInCart) {
         return currentCart.map((item) =>
           item.product.name === product.name
             ? { ...item, quantity: item.quantity + quantity }
-            : item
+            : item,
         );
       } else {
         return [...currentCart, { product, quantity }];
@@ -55,12 +58,14 @@ const SelectComponent = () => {
 
   const removeFromCart = (product: Product) => {
     setCart((currentCart) => {
-      const itemInCart = currentCart.find((item) => item.product.name === product.name);
+      const itemInCart = currentCart.find(
+        (item) => item.product.name === product.name,
+      );
       if (itemInCart && itemInCart.quantity > 1) {
         return currentCart.map((item) =>
-            item.product.name === product.name
-                ? { ...item, quantity: item.quantity - 1 }
-                : item
+          item.product.name === product.name
+            ? { ...item, quantity: item.quantity - 1 }
+            : item,
         );
       } else if (itemInCart && itemInCart.quantity === 1) {
         return currentCart.filter((item) => item.product.name !== product.name);
@@ -75,7 +80,10 @@ const SelectComponent = () => {
   };
 
   const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.product.price * item.quantity, 0);
+    return cart.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0,
+    );
   };
 
   const handleClick = async () => {
@@ -99,47 +107,132 @@ const SelectComponent = () => {
   return (
     <section className="container mx-auto px-4 md:px-8 py-8">
       <div className="w-full flex justify-start space-x-4 overflow-x-auto pb-2 border-b-2">
-        <button className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700">お菓子</button>
-        <button className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700">お肉</button>
-        <button className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700">野菜</button>
+        <button
+          className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700"
+          onClick={() => setSelectedTab("お菓子")}
+        >
+          お菓子
+        </button>
+        <button
+          className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700"
+          onClick={() => setSelectedTab("お肉")}
+        >
+          お肉
+        </button>
+        <button
+          className="py-2 px-4 rounded-lg bg-gray-200 dark:bg-gray-700"
+          onClick={() => setSelectedTab("野菜")}
+        >
+          野菜
+        </button>
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
-        <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
-          <div className="flex items-center justify-between">
-            <h2 className="font-semibold">きのこの山</h2>
-            <Button variant="outlined" onClick={() => addToCart({ name: 'きのこの山', price: 130 })}>カートに追加</Button>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            クラッカーのチョコ掛け
-          </p>
-          <h3 className="font-bold text-gray-900 dark:text-white">130円</h3>
-        </div>
-        <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
-          <div className="flex items-center justify-between">
-            <h2 className="font-semibold">たけのこの里</h2>
-            <Button variant="outlined" onClick={() => addToCart({ name: 'たけのこの里', price: 130 })}>カートに追加</Button>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            ビスケットのチョコ掛け
-          </p>
-          <h3 className="font-bold text-gray-900 dark:text-white">130円</h3>
-        </div>
-        <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
-          <div className="flex items-center justify-between">
-            <h2 className="font-semibold">アルフォート</h2>
-            <Button variant="outlined" onClick={() => addToCart({ name: 'アルフォート', price: 110 })}>カートに追加</Button>
-          </div>
-          <p className="text-gray-500 dark:text-gray-400">
-            ビスケットのチョコ掛け
-          </p>
-          <h3 className="font-bold text-gray-900 dark:text-white">110円</h3>
-        </div>
+        {selectedTab === "お菓子" && (
+          <>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">きのこの山</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() => addToCart({ name: "きのこの山", price: 130 })}
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">
+                クラッカーのチョコ掛け
+              </p>
+              <h3 className="font-bold text-gray-900 dark:text-white">130円</h3>
+            </div>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">たけのこの里</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() =>
+                    addToCart({ name: "たけのこの里", price: 130 })
+                  }
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">
+                ビスケットのチョコ掛け
+              </p>
+              <h3 className="font-bold text-gray-900 dark:text-white">130円</h3>
+            </div>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">アルフォート</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() =>
+                    addToCart({ name: "アルフォート", price: 110 })
+                  }
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">
+                ビスケットのチョコ掛け
+              </p>
+              <h3 className="font-bold text-gray-900 dark:text-white">110円</h3>
+            </div>
+          </>
+        )}
+        {selectedTab === "お肉" && (
+          <>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">牛バラ肉</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() => addToCart({ name: "牛バラ肉", price: 200 })}
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">牛のバラ肉</p>
+              <h3 className="font-bold text-gray-900 dark:text-white">200円</h3>
+            </div>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">豚こま切れ肉</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() =>
+                    addToCart({ name: "豚こま切れ肉", price: 180 })
+                  }
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">豚のこま切れ肉</p>
+              <h3 className="font-bold text-gray-900 dark:text-white">180円</h3>
+            </div>
+            <div className="border border-gray-200 p-4 rounded-lg dark:border-gray-800">
+              <div className="flex items-center justify-between">
+                <h2 className="font-semibold">手羽先</h2>
+                <Button
+                  variant="outlined"
+                  onClick={() => addToCart({ name: "手羽先", price: 130 })}
+                >
+                  カートに追加
+                </Button>
+              </div>
+              <p className="text-gray-500 dark:text-gray-400">鶏の手羽先</p>
+              <h3 className="font-bold text-gray-900 dark:text-white">130円</h3>
+            </div>
+          </>
+        )}
       </div>
       <section className="w-full mt-8">
         <div className="bg-gray-100 p-4 rounded-lg dark:bg-gray-800">
           <div className="flex items-center justify-between">
             <h2 className="font-semibold">カート</h2>
-            <Button variant="contained" onClick={resetCart}>カートのリセット</Button>
+            <Button variant="contained" onClick={resetCart}>
+              カートのリセット
+            </Button>
           </div>
           {/*
           <div className="mt-4">
@@ -150,18 +243,31 @@ const SelectComponent = () => {
           */}
           <div className="mt-4">
             {cart.map((item, index) => (
-                <div key={item.product.name} className={`flex flex-col justify-between items-start ${index < cart.length - 1 ? 'border-b border-gray-200' : ''}`}>
-                  <p className="flex-grow">
-                    {item.product.name} - {item.quantity}個
-                  </p>
-                  <Button className="mt-2" variant="outlined" onClick={() => removeFromCart(item.product)}>数量を減らす</Button>
-                </div>
+              <div
+                key={item.product.name}
+                className={`flex flex-col justify-between items-start ${
+                  index < cart.length - 1 ? "border-b border-gray-200" : ""
+                }`}
+              >
+                <p className="flex-grow">
+                  {item.product.name} - {item.quantity}個
+                </p>
+                <Button
+                  className="mt-2"
+                  variant="outlined"
+                  onClick={() => removeFromCart(item.product)}
+                >
+                  数量を減らす
+                </Button>
+              </div>
             ))}
           </div>
         </div>
         <div className="mt-4 bg-gray-100 p-4 rounded-lg dark:bg-gray-800">
           <h2 className="font-semibold">合計金額</h2>
-          <h3 className="font-bold text-gray-900 dark:text-white">{calculateTotal()}円</h3>
+          <h3 className="font-bold text-gray-900 dark:text-white">
+            {calculateTotal()}円
+          </h3>
         </div>
         <div className="mt-4 flex justify-end">
           <Link href={"/"}>
